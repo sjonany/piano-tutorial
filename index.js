83439e83176a41c2e26ec39a95d015b8fad4035b
Stephen Jonany <sjonany@gmail.com>
2024-10-29 09:03:03 -0700

    Made pagination bookmarkable

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // of the loop. 
 // This is a workaround for https://github.com/cifkao/html-midi-player/issues/23
 const HAS_FINISHED = "has_finished";
+// The total number of pages.
+const NUM_PAGES = 4;
 
 $(document).ready(function() {
     $("midi-player").on("start", function(evt) {
@@ -20,3 +22,41 @@ $(document).ready(function() {
         }
     });
 });
+
+/* Handles pagination logic w/ alpine-js. */
+function pagination() {
+    return {
+        currentPage: 1,
+        totalPages: NUM_PAGES,
+
+        init() {
+            // Get the page number from the URL, or default to the first page.
+            const urlParams = new URLSearchParams(window.location.search);
+            const page = parseInt(urlParams.get('page'));
+            this.currentPage = this.isValidPage(page) ? page : 1;
+        },
+
+        changePage(newPage) {
+            if (!this.isValidPage(newPage)) {
+                return;
+            }
+            
+            this.currentPage = newPage;
+            const newUrl = `${window.location.pathname}?page=${newPage}`;
+            // Change the URL as well so user can bookmark.
+            history.pushState({}, '', newUrl);
+        },
+
+        handleKeydown(event) {
+            if (event.key === 'ArrowLeft') {
+              this.changePage(this.currentPage - 1);
+            } else if (event.key === 'ArrowRight') {
+              this.changePage(this.currentPage + 1);
+            }
+        },
+
+        isValidPage(page) {
+            return page && page >= 1 && page <= this.totalPages;
+        }
+    };
+} 
